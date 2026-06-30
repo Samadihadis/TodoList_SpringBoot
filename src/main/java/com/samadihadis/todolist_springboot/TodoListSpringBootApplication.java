4802ff36cff2d5c6b895b1ca8825b610c2e7c8db
Hadis Samadi <samadi.hadis@gmail.com>
2026-06-30 16:24:13 +0330

    Initial commit

---
+++ src/main/java/com/samadihadis/todolist_springboot/TodoListSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.samadihadis.todolist_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samadihadis/todolist_springboot/TodoListSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samadihadis.todolist_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
