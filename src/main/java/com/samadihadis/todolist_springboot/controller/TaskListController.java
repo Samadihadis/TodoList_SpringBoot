f183980d3c369ce522c9cfda2797cc82a9564b37
Hadis Samadi <samadi.hadis@gmail.com>
2026-07-02 13:12:15 +0330

    Add task list controller

---
+++ src/main/java/com/samadihadis/todolist_springboot/controller/TaskListController.java
@@ -0,0 +1,42 @@
+package com.samadihadis.todolist_springboot.controller;
+
+
+import com.samadihadis.todolist_springboot.entity.TaskList;
+import com.samadihadis.todolist_springboot.service.TaskListService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/taskLists")
+public class TaskListController {
+
+    private final TaskListService taskListService;
+
+    @PostMapping
+    public ResponseEntity<TaskList> createTaskList(@RequestBody TaskList taskList) {
+        TaskList savedTaskList = taskListService.createTaskList(taskList);
+        return ResponseEntity.ok(savedTaskList);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskList>> getAllTaskLists() {
+        return ResponseEntity.ok(taskListService.getAllTaskLists());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskList> getTaskListById(@PathVariable Long id) {
+        return ResponseEntity.ok(taskListService.getTaskListById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteTaskList(@PathVariable Long id) {
+        taskListService.deleteTaskList(id);
+        return ResponseEntity.ok(
+                String.format("لیست کار با شناسه %d حذف شد.", id)
+        );
+    }
+}
